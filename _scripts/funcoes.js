e7d14e6cd5ee641c9e99eca19545e1a0a132fbd6
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2021-02-27 23:23:19 -0300

    add formulario

--- _scripts/funcoes.js
+++ _scripts/funcoes.js
@@ -1,3 +1,8 @@
 function mudaFoto(foto){
     document.getElementById("icone").src = foto;
 }
+function calc_total() {
+    var qtd = parseInt(document.getElementById('iQtd').value);
+    tot = qtd * 1500;
+    document.getElementById('iTot').value = tot;
+}
