480d386afcfedc370e23cec0e17e88142e3fe2f4
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2021-03-19 04:40:15 -0300

    add estruturas for

--- _cursoJS/aula13/repeticao.js
+++ _cursoJS/aula13/repeticao.js
@@ -19,3 +19,12 @@
 // for (var c = 1; c <= 10; c++) {
 //     console.log(`Passo ${c}`)
 // }
+
+// for (let pos in num) { //modo simplificado de exibir o array
+//     console.log(num[pos]);
+// }
+
+// var vetor = [10, 09, 08, 07, 06] //A cada iteração, 'valor' recebe um elemento do array
+// for (let valor of vetor) {
+//     console.log(valor);    
+// }
